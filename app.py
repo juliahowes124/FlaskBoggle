d8268382fdab274141cc2d815aaa006bd214a244
Sam Merham <samehisaac@yahoo.com>
2021-03-05 15:40:34 -0800

    Step1 -Step4

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, jsonify, session
 from uuid import uuid4
-
+from wordlist import english_words
 from boggle import BoggleGame
 
 app = Flask(__name__)
@@ -20,10 +20,24 @@ def homepage():
 @app.route("/api/new-game")
 def new_game():
     """Start a new game and return JSON: {game_id, board}."""
-
+    
     # get a unique id for the board we're creating
     game_id = str(uuid4())
     game = BoggleGame()
     games[game_id] = game
 
-    return {"gameId": "need-real-id", "board": "need-real-board"}
+    return {"gameId": game_id, "board": game.board}
+
+@app.route('/api/score-word', methods=["POST"])
+def score_word():
+    data = request.json
+    id = data['gameId']
+    print(id)
+    word = data['word']
+    result = 'ok'
+    if word not in english_words.words:
+        result = 'not-word'
+    if not games[id].check_word_on_board(word):
+        result = 'not-on-board'
+    return jsonify({'result': result})
+
