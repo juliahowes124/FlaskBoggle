42a7de1cf833a27d58245bff2d3a3230e8c5b03e
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-03-28 15:53:23 -0700

    styling and merge bottom alert messages with top alert

--- static/boggle.js
+++ static/boggle.js
@@ -3,23 +3,23 @@
 const $playedWords = $("#word_list");
 const $form = $("#newWordForm");
 const $wordInput = $("#wordInput");
-const $message = $(".msg");
 const $table = $("table");
-const $gameMsg= $(".game-msg");
+const $gameMsg = $(".game-msg");
 const $timer = $(".timer");
 const $endGame = $('.end-game-msg');
 
 let gameId;
 let gameOver = false;
-
-/** Start */
+let okTimeout = null;
+let errTimeout = null;
 
 async function start() {
+  $gameMsg.text('Go!')
   let response = await axios.get("/api/new-game");
   gameId = response.data.gameId;
   let board = response.data.board;
   displayBoard(board);
-  let time = 10;
+  let time = 15;
   $timer.text(`Time Left: ${time}s`);
   const timer = window.setInterval(async () => {
     time--;
@@ -32,6 +32,10 @@ async function start() {
 }
 
 async function endGame() {
+  clearTimeout(errTimeout);
+  clearTimeout(okTimeout);
+  console.log('err: ', errTimeout);
+  console.log('ok:', okTimeout);
   let response = await axios.post('/api/end-game', {
     gameId
   })
@@ -39,14 +43,50 @@ async function endGame() {
   let numWords = response.data.num_words;
   let highScore = response.data.high_score;
   let highScoreNumWords = response.data.high_score_num_words;
-  $gameMsg.removeClass('hidden');
   $gameMsg.text(`Final Score: ${finalScore}pts
                   with ${numWords} words --- High Score: 
                   ${highScore}pts with ${highScoreNumWords} words`);
-  $message.empty()
-  $message.addClass('hidden')
+  $gameMsg.removeClass('alert-danger', 'alert-succes').addClass('alert-primary')
   gameOver = true;
+  $wordInput.prop( "disabled", true );
+  $wordInput.val('');
 }
+
+$form.on('submit', async (e) => {
+  e.preventDefault();
+  if (gameOver) return;
+  let word = $wordInput.val();
+  $wordInput.val('');
+  let response = await axios.post('/api/score-word', {
+    gameId,
+    word
+  })
+  if (response.data["result"] === "ok") {
+    clearTimeout(errTimeout);
+    clearTimeout(okTimeout);
+    console.debug(errTimeout);
+    $playedWords.append(`<p>${word}</p>`)
+    $gameMsg.text(`${word.toUpperCase()} added! Score: ${response.data.score}`)
+    $gameMsg.removeClass(['alert-primary', 'alert-danger']).addClass('alert-success')
+    okTimeout = window.setTimeout(() => {
+      console.debug('ok timeout')
+      $gameMsg.text(`Score: ${response.data.score}`)
+      $gameMsg.removeClass(['alert-success']).addClass('alert-primary')
+    }, 2000)
+    $gameMsg.removeClass('hidden');
+  } else {
+    clearTimeout(errTimeout);
+    clearTimeout(okTimeout);
+    $gameMsg.text(`${response.data["result"]} -- Score: ${response.data.score}`)
+    $gameMsg.removeClass(['alert-primary', 'alert-success']).addClass('alert-danger')
+    errTimeout = window.setTimeout(() => {
+      console.debug('err timeout');
+      $gameMsg.text(`Score: ${response.data.score}`)
+      $gameMsg.removeClass(['alert-danger']).addClass('alert-primary')
+    }, 2000)
+    $gameMsg.removeClass('hidden');
+  }
+})
 
 /** Display board */
 
@@ -61,30 +101,5 @@ function displayBoard(board) {
     $table.append($row)
   }
 }
-
-$form.on('submit', async (e) => {
-  e.preventDefault();
-  if (gameOver) return;
-  let word = $wordInput.val();
-  $wordInput.val('');
-  let response = await axios.post('/api/score-word', {
-    gameId,
-    word
-  })
-  if (response.data["result"] === "ok") {
-    $playedWords.append(`<p>${word}</p>`)
-    $gameMsg.text(`Score: ${response.data.score}`)
-    $message.empty()
-    $message.append(`${word.toUpperCase()} added!`)
-    $message.removeClass('alert-danger')
-    $message.addClass('alert-success')
-    $gameMsg.removeClass('hidden');
-  } else {
-    $message.empty()
-    $message.append(response.data["result"])
-    $message.removeClass('alert-success')
-    $message.addClass('alert-danger')
-  }
-})
 
 start();
