9a58392765a0f070ad7efaa13c88029f6187c5a9
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-06-13 16:20:20 -0700

    wrapped up UI testing

--- static/boggle.test.js
+++ static/boggle.test.js
@@ -1,22 +1,39 @@
 let arr;
-beforeEach(function(){
+
+beforeAll(() => {
   jasmine.getFixtures().fixturesPath = '';
   loadFixtures('../templates/index.html')
+  spyOn(axios, 'get').and.returnValue({
+    data: {
+      gameId: 1,
+      board: [
+        ["C", "A", "T", "S", "A"]
+      ]
+    }
+  });
+  spyOn(axios, 'post').and.returnValue({
+    data: {
+      score: 1,
+      num_words: 1,
+      high_score: 6,
+      high_score_num_words: 3
+    }
+  });
 });
 
-
 describe("Boggle", function(){
-  it("starts game", async function(){
-    spyOn(axios, 'get').and.returnValue({
-      data: {
-        gameId: 1,
-        board: [
-          ["C", "A", "T", "S", "A"]
-        ]
-      }
-    });
+  it("starts and ends game", async function(){
     await start();
     expect(axios.get).toHaveBeenCalled();
     expect($('table')).toHaveHtml("<tr><td>C</td><td>A</td><td>T</td><td>S</td><td>A</td></tr>")
+    
+    var spyEvent = spyOnEvent($('#newWordForm'), 'submit');
+    $("#wordInput").val('CAT');
+    $("#newWordBtn").trigger("click");
+    expect(spyEvent).toHaveBeenTriggered()
+    
+    await endGame();
+    expect(axios.post).toHaveBeenCalled();
+    expect($(".game-msg")).toHaveText("Final Score: 1pts\n                  with 1 words --- High Score: \n                  6pts with 3 words");
   });
 });
